e5b8ebf8872e2a36043ba13b6a7d856503031fdc
kim henry <kimanihenry14489@gmail.com>
2025-01-24 08:41:52 +0300

    Styled the site to match the orange-ish theme asthetics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
+import { sidebar } from './astro.sidebar';
+import { devServerFileWatcher } from './config/dev-server-file-watcher';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs with Tailwind',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: ['./src/tailwind.css'],
-		}),
-		tailwind({ applyBaseStyles: false }),
-	],
+  integrations: [devServerFileWatcher([
+    './config/*', // custom functions 
+    './astro.sidebar.ts', // Sidebar configuration file
+    './src/content/nav/*.ts' // Sidebar labels
+  ]), starlight({
+    title: 'Egg',
+    components: {
+      Sidebar: "./src/components/starlight/Sidebar.astro",
+    },
+    social: {
+      github: 'https://github.com/withastro/starlight',
+    },
+    sidebar,
+    customCss: [
+      './src/tailwind.css',
+      '@fontsource-variable/nunito',
+      '@fontsource/poppins',
+      '@fontsource/roboto',
+      '@fontsource-variable/inter',
+      '@fontsource-variable/fredoka',
+      '@fontsource-variable/quicksand',
+      '@fontsource-variable/noto-sans-lao',
+    ],
+  }), tailwind({ applyBaseStyles: false }), react()],
 });
