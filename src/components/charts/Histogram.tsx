0ee8303d01414b9b8a08b3d748df7aa9ed30da31
kim henry <kimanihenry14489@gmail.com>
2025-02-12 21:29:37 +0300

    Component: Histogram Graph -> 2

--- src/components/charts/Histogram.tsx
+++ src/components/charts/Histogram.tsx
@@ -1,53 +1,87 @@
 import { createHistogramHeight } from "@/lib/charts"; 
-
 type HistogramComponentProps = {
-  classBoundary: string;
-  classInterval: number;
+  classRange: string; // 100 -104
   frequency: number[];
+  classInterval: number;
 };
 
-// The structure of the data required to draw the histrogram
-type HistogramDataType = {
-  height: number;
-  classbond: string;
-}
 // Logic to draw the histogram
 export default function Histogram({
-  classBoundary, frequency, classInterval 
+  classRange , frequency, classInterval
 }: HistogramComponentProps) {
+  const lowerBound = parseInt(classRange.split("-")[0]) - 0.5;
+  const maxHeight = maxGraphHeight(frequency, classInterval)
 
+  const BarElements = histogramData({ classInterval, frequency, lowerBound, maxHeight }).map(bar => {
+    return <Bar height={bar.height} xValue={bar.upperBound} maxHeight={maxHeight} />
+  });
+  
   return (
-    <div className="flex bg-[var(--sl-color-gray-6)] w-full min-h-[200px] h-[400px]">
-      <Bar height={200} xValue={99.5} />
-      <Bar height={100} xValue={104.5}/>
-      <Bar height={150} xValue={109.5}/>
+    <div className="flex bg-[var(--sl-color-gray-6)] w-full min-h-[200px] h-[400px] overflow-auto">
+      <YAxis maxHeight={maxHeight} />
+      {BarElements}
     </div>
   );
 }
 
-// Generate the data required to create the histogram
-function histogramData({ classInterval, classBoundary, frequency}: HistogramComponentProps) {
-  const histogramData: HistogramDataType[] = [];
+function YAxis({ maxHeight }: { maxHeight: number}) {
+  const styles = {
+    height: Math.round(maxHeight)*100 + "px",
+  }
 
-  return 2;
+  return (
+    <div className="grid content-end !m-0 min-w-14 w-20 h-full">
+      <div className="w-full" style={styles}></div>
+      <div className="w-full h-10"></div>
+    </div>
+  );
 }
 
+// Render the bars
 function Bar({
-  height, xValue
-}: { height: number, xValue: number }) {
+  height, xValue, maxHeight
+}: { height: number, xValue: number, maxHeight: number }) {
 
   const styles = {
-    height: height + "px",
+    // applify the height of the bars
+    height: height*maxHeight + "px" ,
   };
   return (
-    <div className="grid !m-0 content-end h-full w-20" >
-      <div className="w-full border-t border-l border-r border-[var(--sl-color-gray-2)]" style={styles}></div>
+    <div className="grid !m-0 content-end h-full min-w-14 w-20">
+      <div className="w-full border-t border-l border-r border-[var(--sl-color-gray-2)]" style={styles} ></div>
+
+      {/* x-axis value*/}
       <div className="relative !m-0 w-full h-10 border-t-[var(--sl-color-gray-2)] border-t-2">
         {/* pointer for the x-axis scale */}
         <div className="absolute !m-0 top-0 -right-[1px] w-0.5 h-2 z-10 bg-[var(--sl-color-gray-2)]"></div>
         {/* the x-axis value */}
         <div className="absolute !m-0 bottom-1 -right-1/4 z-10">{xValue}</div>
       </div>
+
     </div> 
   );
+}
+
+// Generate the data required to create the histogram
+// Height: as a percentage
+// xValue: the values used on the x-axis
+function histogramData(
+  {  classInterval, frequency, lowerBound, maxHeight }:
+  { classInterval: number, frequency: number[], lowerBound: number, maxHeight: number}
+) {
+
+  return frequency.map(freq => {
+    const height = createHistogramHeight({ freq, classInterval });
+    lowerBound += 5;
+    return {
+      height: height/Math.round(maxHeight)*100, //return the heights as percentages
+      upperBound: lowerBound, // xValues
+    }
+  });
+}
+
+// return the maximum height of the graph
+function maxGraphHeight(frequency: number[], classInterval: number) {
+  let maxFreq = Math.max(...frequency);
+  return createHistogramHeight({ freq: maxFreq, classInterval});
 }
