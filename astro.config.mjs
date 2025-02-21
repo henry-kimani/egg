a2493b797b86b98f3a266f11f48a1738e532fd39
kim henry <kimanihenry14489@gmail.com>
2025-02-21 23:38:42 +0300

    [library]: new library for adding inline LaTex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,27 @@ import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 import { sidebar } from './astro.sidebar';
 import { devServerFileWatcher } from './config/dev-server-file-watcher';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [rehypeKatex, { /* Katex plugin options */ }]
+    ],
+  },
   integrations: [devServerFileWatcher([
     './config/*', // custom functions 
     './astro.sidebar.ts', // Sidebar configuration file
     './src/content/nav/*.ts' // Sidebar labels
   ]),
   starlight({
-    title: '🥚Egg',
+      title: '🥚Egg',
+      favicon: "/egglogo.svg",
     components: {
       Hero: "./src/components/starlight/Hero.astro",
       Sidebar: "./src/components/starlight/Sidebar.astro",
