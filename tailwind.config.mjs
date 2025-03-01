31c16c97d0f9983540d600af5b8e91a6b6393b35
kim henry <kimanihenry14489@gmail.com>
2025-03-01 23:49:38 +0300

    + ColorScheme for the site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,25 @@
 import colors from 'tailwindcss/colors';
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
+// Generated color palettes
+const accent = { 200: '#eebe95', 600: '#ea580c', 900: '#4c2800', 950: '#381c00' };
+const gray = { 100: '#f7f6f5', 200: '#f0edeb', 300: '#c4c1bf', 400: '#908a85', 500: '#5c5753', 700: '#3c3733', 800: '#2b2622', 900: '#1a1816' };
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			colors: {
-				// Your preferred accent color. Indigo is closest to Starlight’s defaults.
-				accent: colors.indigo,
-				// Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-				gray: colors.zinc,
-			},
+        accent,
+        gray,
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      }
 		},
 	},
 	plugins: [starlightPlugin()],
