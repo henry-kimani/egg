4b723ac14c0f70e81019fa695af8f30a7d22967a
kim henry <kimanihenry14489@gmail.com>
2025-03-04 22:57:56 +0300

    Small changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import starlightPlugin from '@astrojs/starlight-tailwind';
 
 // Generated color palettes
-const accent = { 200: '#eebe95', 600: '#ea580c', 900: '#4c2800', 950: '#381c00' };
+const accent = { 200: '#eebe95', 600: '#eb5b00', 900: '#4c2800', 950: '#381c00' };
 const gray = { 100: '#f7f6f5', 200: '#f0edeb', 300: '#c4c1bf', 400: '#908a85', 500: '#5c5753', 700: '#3c3733', 800: '#2b2622', 900: '#1a1816' };
 
 /** @type {import('tailwindcss').Config} */
