2a8db792afa1fcf814e9b16b93363a26af4af931
kim henry <kimanihenry14489@gmail.com>
2025-03-11 23:59:42 +0300

    Added the release page

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,21 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
+import { glob } from 'astro/loaders';
 
-export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-};
+const releaseNotes = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/releaseNotes"}),
+  schema: docsSchema({
+    extend: z.object({
+      version: z.string(),
+      features: z.array(z.string())
+    })
+  }),
+});
+
+const docs = defineCollection({
+  loader: docsLoader(), 
+  schema: docsSchema(), 
+})
+
+export const collections = { docs, releaseNotes };
