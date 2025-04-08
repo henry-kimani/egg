39c33e9997565327c9cc15794f7fdf1e31604947
kim henry <kimanihenry14489@gmail.com>
2025-04-08 09:44:42 +0300

    Added firebase server credentials to add SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import { devServerFileWatcher } from "./config/dev-server-file-watcher";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
@@ -15,37 +17,42 @@ export default defineConfig({
       [rehypeKatex, { /* Katex plugin options */ }]
     ],
   },
-	integrations: [
-    devServerFileWatcher([
-      './config/*',
-      './astro.sidebar.ts',
-      './src/content/nav/*.ts'
-    ]),
-		starlight({
-      title: 'Egg',
-      logo: {
-        src: "./src/assets/egglogo.png",
-        alt: "Egg's Logo",
-      },
-      components: {
-        Hero: "./src/components/starlight/Hero.astro",
-        Footer: "./src/components/starlight/Footer.astro",
-        Sidebar: "./src/components/starlight/Sidebar.astro",
-        Header: "./src/components/starlight/Header.astro",
-        MobileMenuFooter: "./src/components/starlight/MobileMenuFooter.astro",
-      },
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar,
-      customCss: [
-        './src/tailwind.css',
-        '@fontsource-variable/quicksand',
-        '@fontsource-variable/fredoka',
-        '@fontsource-variable/noto-sans-lao'
-      ],
-      favicon: "egglogo.svg",
-		}),
-		tailwind({ applyBaseStyles: false }),
+
+  integrations: [
+  devServerFileWatcher([
+    './config/*',
+    './astro.sidebar.ts',
+    './src/content/nav/*.ts'
+  ]),
+      starlight({
+    title: 'Egg',
+    logo: {
+      src: "./src/assets/egglogo.png",
+      alt: "Egg's Logo",
+    },
+    components: {
+      Hero: "./src/components/starlight/Hero.astro",
+      Footer: "./src/components/starlight/Footer.astro",
+      Sidebar: "./src/components/starlight/Sidebar.astro",
+      Header: "./src/components/starlight/Header.astro",
+      MobileMenuFooter: "./src/components/starlight/MobileMenuFooter.astro",
+    },
+          social: {
+              github: 'https://github.com/withastro/starlight',
+          },
+          sidebar,
+    customCss: [
+      './src/tailwind.css',
+      '@fontsource-variable/quicksand',
+      '@fontsource-variable/fredoka',
+      '@fontsource-variable/noto-sans-lao'
+    ],
+    favicon: "egglogo.svg",
+      }),
+      tailwind({ applyBaseStyles: false }),
 	],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
