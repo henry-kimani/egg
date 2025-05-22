2feba64c9afda11c9e2157581655dee8be70a06f
kim henry <kimanihenry14489@gmail.com>
2025-05-22 23:03:29 +0300

    Update sign-in sign-out button in the profile dropdown

--- src/components/buttons/SimpleButton.tsx
+++ src/components/buttons/SimpleButton.tsx
@@ -1,17 +1,19 @@
 import { cn } from "@lib/utils";
 
 interface Props {
+  id?: string;
+  icon?: string;
   children: string;
   class?: string;
 };
 
-export default function SimpleButton({ children, class: styles }: Props) {
+export default function SimpleButton({ children, id, icon, class: styles }: Props) {
   return (
-    <button 
+    <button id={id}
       class={cn(`cursor-pointer px-3 !border-none py-1 focus:ring focus:outline-none focus:ring-[var(--sl-color-accent)] focus:ring-offset-2 dark:focus:ring-offset-[var(--sl-color-gray-5)]
       flex gap-2 items-center rounded-md bg-[var(--sl-color-accent)] text-[var(--sl-color-black)] dark:bg-[var(--sl-color-accent-high)]
     `, styles)}>
-      {children}
+      {children} {icon}
     </button>
   );
-}
+};
