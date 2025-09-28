2421d32b990e373d44f6f340d7c33bf89a2022e9
kim henry <kimanihenry14489@gmail.com>
2025-09-28 23:31:32 +0300

    Used the recommended nodejs version by firebase

--- shell.nix
+++ shell.nix
@@ -1,8 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
 
 pkgs.mkShell {
   packages = [
-    pkgs.nodejs_22
+    pkgs.nodejs_20
     pkgs.firebase-tools
     pkgs.pnpm
     pkgs.jdk # For firebase emulators
