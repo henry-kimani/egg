335ca4e9a487f70067fa10ee5d87df123b840d83
kim henry <kimanihenry14489@gmail.com>
2025-11-06 23:51:14 +0300

    Isolated all the regex expressions

---
+++ src/lib/regex.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Match all the paths for courses */
+export const coursesRoutes = /^\/year.*/i;
+
+/**
+ * Match docs route */
+export const docsRoute = /^\/docs.*/i;
+
+/** 
+ * Match the first character of a string */
+export const matchFirstChar = /^[\w]/i;
